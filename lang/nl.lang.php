caef28c4813f4318a89e02bb87479d0c6fc48fd3
albert810 <ahwildeboer@gmail.com>
2016-11-17 17:41:03 +0100

    vertaling   , de nederlandse gedeelte  , hij is nog niet af , heb alleen maar een begin gemaakt, ik mis nog informatie

--- lang/nl.lang.php
+++ lang/nl.lang.php
@@ -1,8 +1,35 @@
 <?php
 
-$lang = array (
-	'index_titel' => 'BEMA Wegenbouw BV',
-	
-)
+
+//startpagina vertaling nederlands
+$langNederlands = array (
+        //over ons en titel
+    'index_titel'                   => 'BEMA Wegenbouw BV',
+    "slogan"                        => "Kwaliteit staat voorop",
+    "over ons kopje"                => "Over Ons Bedrijf",
+    "tekst van over ons"            => ""  , //dit moet nog gedaan worden!! onder het over ons
+    "tekst van over ons 2"          => "", //dit moet nog gedaan worden!! onder het over ons
+   
+    
+     //diensten
+    "diensten kopje "               =>"diensten",
+    "diensten onder kopje"          => "De volgende diensten bieden wij aan",
+    
+    "grondwerk"                     => "Grondwerk",
+    "tekst onder grondwerk"         => "",                 //dit moet nog gedaan worden!! 
+    
+    "Riolering"                     =>"Riolering",
+    "tekst onder Riolering "        =>"",         //dit moet nog gedaan worden!! 
+ 
+      
+    "Machinale bestrating"          =>"Machinale bestrating",
+     "tekst onder machinale bestrating "  =>"",      //dit moet nog gedaan worden!! 
+    
+    "uitvlakken"                    =>"Uitvlakken",
+     "tekst onder uitvlakken"       =>""                    //dit moet nog gedaan worden!! 
+   
+   
+    
+);
 
 ?>
