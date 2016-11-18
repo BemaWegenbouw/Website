7cd1f34fed29e7a07b08ea3bcb335378f20e0ced
albert810 <ahwildeboer@gmail.com>
2016-11-18 10:40:01 +0100

    benaming verbeterd (hij is nog niet klaar er komen nog andere teksten etc bij)

--- lang/nl.lang.php
+++ lang/nl.lang.php
@@ -1,35 +1,42 @@
 <?php
 
-
+//is nog niet klaar!
 //startpagina vertaling nederlands
 $langNederlands = array (
         //over ons en titel
     'index_titel'                   => 'BEMA Wegenbouw BV',
-    "slogan"                        => "Kwaliteit staat voorop",
-    "over ons kopje"                => "Over Ons Bedrijf",
-    "tekst van over ons"            => ""  , //dit moet nog gedaan worden!! onder het over ons
-    "tekst van over ons 2"          => "", //dit moet nog gedaan worden!! onder het over ons
+    "index_slogan"                        => "Kwaliteit staat voorop",
+    "index_kopje"                => "Over Ons Bedrijf",
+    "index_bedrijfsinfo"            => ""  , //dit moet nog gedaan worden!! onder het over ons
+    "index_bedrijfsinfo2"          => "", //dit moet nog gedaan worden!! onder het over ons
    
     
      //diensten
-    "diensten kopje "               =>"diensten",
-    "diensten onder kopje"          => "De volgende diensten bieden wij aan",
+    "index_dienstenkopje "               =>"diensten",
+    "index_diensten"          => "De volgende diensten bieden wij aan",
     
-    "grondwerk"                     => "Grondwerk",
-    "tekst onder grondwerk"         => "",                 //dit moet nog gedaan worden!! 
+    "index_grondwerk"                     => "Grondwerk",
+    "index_tekstGrondwerk"         => "",                 //dit moet nog gedaan worden!! 
     
-    "Riolering"                     =>"Riolering",
-    "tekst onder Riolering "        =>"",         //dit moet nog gedaan worden!! 
+    "index_Riolering"                     =>"Riolering",
+    "index_tekstRiolering "        =>"",         //dit moet nog gedaan worden!! 
  
       
-    "Machinale bestrating"          =>"Machinale bestrating",
-     "tekst onder machinale bestrating "  =>"",      //dit moet nog gedaan worden!! 
+    "index_Machinale bestrating"          =>"Machinale bestrating",
+     "index_tekstMachinalebestrating "  =>"",      //dit moet nog gedaan worden!! 
     
-    "uitvlakken"                    =>"Uitvlakken",
-     "tekst onder uitvlakken"       =>""                    //dit moet nog gedaan worden!! 
+    "index_uitvlakken"                    =>"Uitvlakken",
+     "index_tekstUitvlakken"       =>""                    //dit moet nog gedaan worden!! 
    
    
-    
+//de rest komt nog    
 );
+
+
+
+
+
+
+
 
 ?>
