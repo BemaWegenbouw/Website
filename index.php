00dfdddb7eba3792437b7321430377a5755fcea6
joeyb276 <j.benning@hotmail.nl>
2016-11-20 18:32:56 +0100

    navbar en header php include

--- index.php
+++ index.php
@@ -9,72 +9,12 @@
 
     </head>
     <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+		<!-- Navbar -->
+        <?php include("navbar.php"); ?>
+		<!-- Header -->
+		<?php include("header.php"); ?>
 
-        <nav class="navbar navbar-default transparent navbar-fixed-top">
-            <div class="nav-container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-
-                    </button >
-                    <a class="navbar-brand" href="#myPage" title="To Top"><img src="assets/img/logo.png"></a>
-                </div>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#myPage" title="To Top">Hoofdpagina</a></li>
-                        <li><a href="#overons">Over ons</a></li>
-                        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Diensten <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#diensten">Diensten</a></li>
-                                <li><a href="pages/Grondwerk.php">Grondwerk</a></li>
-                                <li><a href="pages/Riolering.php">Riolering</a></li>
-                                <li><a href="pages/Machinalebestrating.php">Machinale bestrating</a></li>
-                                <li><a href="pages/Uitvlakken.php">Uitvlakken</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="pages/Portfolio.php">Portfolio</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        <li><a href="#googleMap">Locatie</a></li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-
-                        <li><a href="lang/nl.php"><span class="glyphicon glyphicon-log-in"></span> NL</a></li>
-                        <li><a href="lang/en.php"><span class="glyphicon glyphicon-log-in"></span> EN</a></li>
-                        <li><a href="lang/de.php"><span class="glyphicon glyphicon-log-in"></span> DE</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <!--begin bg-carousel-->
-
-        <div id="bg-fade-carousel" class="carousel slide carousel-fade " data-interval="10000"  data-ride="carousel">
-            <!-- Wrapper for slides -->
-            <div class="carousel-inner">
-                <div class="item active">
-                    <div class="slide1"></div>
-                </div>
-                <div class="item">
-                    <div class="slide2"></div>
-                </div>
-                <div class="item">
-                    <div class="slide3"></div>
-                </div>
-            </div><!-- .carousel-inner -->
-            <div class="container carousel-overlay text-center">
-
-                <div class="jumbotron text-center">
-
-                    <h1>Bema Wegenbouw BV</h1>
-                    <p>Kwaliteit staat voorop</p>
-                </div>
-            </div>
-
-        </div>
-        <!-- .carousel -->
-        <!--end bg-carousel-->
+        
 
 
         <!-- Container (About Section) -->
---
+++ navbar.php
@@ -0,0 +1,37 @@
+<nav class="navbar navbar-default transparent navbar-fixed-top">
+            <div class="nav-container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+
+                    </button >
+                    <a class="navbar-brand" href="#myPage" title="To Top"><img src="assets/img/logo.png"></a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#myPage" title="To Top">Hoofdpagina</a></li>
+                        <li><a href="#overons">Over ons</a></li>
+                        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Diensten <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#diensten">Diensten</a></li>
+                                <li><a href="pages/Grondwerk.php">Grondwerk</a></li>
+                                <li><a href="pages/Riolering.php">Riolering</a></li>
+                                <li><a href="pages/Machinalebestrating.php">Machinale bestrating</a></li>
+                                <li><a href="pages/Uitvlakken.php">Uitvlakken</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="pages/Portfolio.php">Portfolio</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#googleMap">Locatie</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+
+                        <li><a href="lang/nl.php"><span class="glyphicon glyphicon-log-in"></span> NL</a></li>
+                        <li><a href="lang/en.php"><span class="glyphicon glyphicon-log-in"></span> EN</a></li>
+                        <li><a href="lang/de.php"><span class="glyphicon glyphicon-log-in"></span> DE</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
