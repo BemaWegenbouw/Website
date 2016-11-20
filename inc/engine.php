64e489f92e0430371649d4b6f3598ae12205bbbd
Josha <hevadejo@outlook.com>
2016-11-20 22:59:42 +0100

    DB begin

--- inc/engine.php
+++ inc/engine.php
@@ -1,6 +1,9 @@
 <?php
 
-// Bestanden importeren
+// Laad configuratie
+require_once("config.php");
+
+//Laad controllers
 require_once("controller/db.controller.php"); //Importeer DB controller
 require_once("controller/user.controller.php"); //Importeer user controller
 require_once("controller/staff.controller.php"); //Importeer staff controller
