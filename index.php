63b8a51bca2e5814903a2b9bb1875a27903e92b5
Josha van der Heide <hevadejo@outlook.com>
2016-11-21 11:32:15 +0100

    Index fix

--- index.php
+++ index.php
@@ -9,11 +9,11 @@ include("inc/parts/header.php");
 
 ?>
 
-<div id="Overons" class="container slideanim">
+<div id="Overons" class="container">
     <div class="row">
         <div class="col-sm-2"></div>
         <div class="col-sm-8">
-            <h2>Wegenbouw op zijn best</h2><br>
+            <h2 class="text-center">Wegenbouw op zijn best</h2><br>
             <h4>Bij Bema Wegenbouw is uw opdracht onze passie. Wij zorgen voor kwalitatief goed werk voor de beste prijzen. Met grote opdrachten in Nederland én Duitsland zijn wij een betrouwbare partner voor al uw wegenbouw zaken.</h4><br>
             <p><?php echo(lang('index_column1_text')); ?></p>
 
