0f3b80d8f65da99f4952a41a8aba5438a5381534
Josha van der Heide <hevadejo@outlook.com>
2016-11-21 13:52:01 +0100

    Index volledig in taalsysteem gezet

--- index.php
+++ index.php
@@ -10,51 +10,51 @@ include("inc/parts/header.php");
 ?>
 
         <div id="diensten" class="container text-center">
-            <h2>DIENSTEN</h2><br>
-            <h4>De volgende diensten bieden wij aan</h4>
+            <h2><?php echo(lang('index_column1_title')); ?></h2><br>
+            <h4><?php echo(lang('index_column1_sub')); ?></h4>
             <div class="row text-center">
-                <div class="col-sm-4" id="Grondwerk">
+                <div class="col-sm-4" id="<?php echo(lang('index_column1_item1_name')); ?>">
                     <div class="thumbnail">
                         <img src="assets/img/img3.jpg" alt="Paris" width="400" height="300">
-                        <p><strong>Grondwerk</strong></p>
-                        <p>Met grondwerk word</p>
+                        <p><strong><?php echo(lang('index_column1_item1_name')); ?></strong></p>
+                        <p><?php echo(lang('index_column1_item1_desc')); ?></p>
 
                     </div>
                 </div>
                 <div class="col-sm-4" id="Riolering">
                     <div class="thumbnail">
-                        <img src="assets/img/img8.jpg" alt="New York" width="400" height="300">
-                        <p><strong>Riolering</strong></p>
-                        <p>We built New York</p>
+                        <img src="assets/img/img8.jpg" alt="<?php echo(lang('index_column1_item1_name')); ?>" width="400" height="300">
+                        <p><strong><?php echo(lang('index_column1_item2_name')); ?></strong></p>
+                        <p><?php echo(lang('index_column1_item2_desc')); ?></p>
                     </div>
                 </div>
                 <div class="col-sm-4" id="Machinalebestrating">
                     <div class="thumbnail">
-                        <img src="assets/img/img13.jpg" alt="San Francisco" width="400" height="300">
-                        <p><strong>Machinale bestrating</strong></p>
-                        <p>Yes, San Fran is ours</p>
+                        <img src="assets/img/img13.jpg" alt="<?php echo(lang('index_column1_item3_name')); ?>" width="400" height="300">
+                        <p><strong><?php echo(lang('index_column1_item3_name')); ?></strong></p>
+                        <p><?php echo(lang('index_column1_item3_desc')); ?></p>
                     </div>
                 </div>
 
                 <div class="col-sm-4" id="Uitvlakken">
                     <div class="thumbnail">
-                        <img src="assets/img/img15.jpg" alt="Paris" width="400" height="300">
-                        <p><strong>Uitvlakken</strong></p>
-                        <p>Yes, we built Paris</p>
+                        <img src="assets/img/img15.jpg" alt="<?php echo(lang('index_column1_item4_name')); ?>" width="400" height="300">
+                        <p><strong><?php echo(lang('index_column1_item4_name')); ?></strong></p>
+                        <p><?php echo(lang('index_column1_item4_desc')); ?></p>
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="thumbnail">
-                        <img src="assets/img/img15.jpg" alt="New York" width="400" height="300">
-                        <p><strong>New York</strong></p>
-                        <p>We built New York</p>
+                        <img src="assets/img/img15.jpg" alt="<?php echo(lang('index_column1_item5_name')); ?>" width="400" height="300">
+                        <p><strong><?php echo(lang('index_column1_item5_name')); ?></strong></p>
+                        <p><?php echo(lang('index_column1_item5_desc')); ?></p>
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="thumbnail">
-                        <img src="assets/img/img15.jpg" alt="San Francisco" width="400" height="300">
-                        <p><strong>San Francisco</strong></p>
-                        <p>Yes, San Fran is ours</p>
+                        <img src="assets/img/img15.jpg" alt="<?php echo(lang('index_column1_item6_name')); ?>" width="400" height="300">
+                        <p><strong><?php echo(lang('index_column1_item6_name')); ?></strong></p>
+                        <p><?php echo(lang('index_column1_item6_desc')); ?></p>
                     </div>
                 </div>
             </div>
--- overons.php
+++ overons.php
@@ -12,9 +12,9 @@ include("inc/parts/header.php");
             <div class="row">
                 <div class="col-sm-2"></div>
                 <div class="col-sm-8">
-                    <h2 class="text-center"><?php echo(lang('index_column1_title')); ?></h2><br>
-                    <h4><?php echo(lang('index_column1_head')); ?></h4><br>
-                    <p><?php echo(lang('index_column1_text')); ?></p>
+                    <h2 class="text-center"><?php echo(lang('overons_column1_title')); ?></h2><br>
+                    <h4><?php echo(lang('overons_column1_head')); ?></h4><br>
+                    <p><?php echo(lang('overons_column1_text')); ?></p>
 
                 </div>
                 <div class="col-sm-2">
