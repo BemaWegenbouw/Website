36767bda5998eacf7d5e5e972fc8a696d9dbf549
Josha van der Heide <hevadejo@outlook.com>
2016-11-21 13:01:46 +0100

    Pagina fixes

--- index.php
+++ index.php
@@ -9,18 +9,55 @@ include("inc/parts/header.php");
 
 ?>
 
-<div id="Overons" class="container">
-    <div class="row">
-        <div class="col-sm-2"></div>
-        <div class="col-sm-8">
-            <h2 class="text-center">Wegenbouw op zijn best</h2><br>
-            <h4>Bij Bema Wegenbouw is uw opdracht onze passie. Wij zorgen voor kwalitatief goed werk voor de beste prijzen. Met grote opdrachten in Nederland én Duitsland zijn wij een betrouwbare partner voor al uw wegenbouw zaken.</h4><br>
-            <p><?php echo(lang('index_column1_text')); ?></p>
+        <div id="diensten" class="container text-center">
+            <h2>DIENSTEN</h2><br>
+            <h4>De volgende diensten bieden wij aan</h4>
+            <div class="row text-center">
+                <div class="col-sm-4" id="Grondwerk">
+                    <div class="thumbnail">
+                        <img src="assets/img/img3.jpg" alt="Paris" width="400" height="300">
+                        <p><strong>Grondwerk</strong></p>
+                        <p>Met grondwerk word</p>
 
+                    </div>
+                </div>
+                <div class="col-sm-4" id="Riolering">
+                    <div class="thumbnail">
+                        <img src="assets/img/img8.jpg" alt="New York" width="400" height="300">
+                        <p><strong>Riolering</strong></p>
+                        <p>We built New York</p>
+                    </div>
+                </div>
+                <div class="col-sm-4" id="Machinalebestrating">
+                    <div class="thumbnail">
+                        <img src="assets/img/img13.jpg" alt="San Francisco" width="400" height="300">
+                        <p><strong>Machinale bestrating</strong></p>
+                        <p>Yes, San Fran is ours</p>
+                    </div>
+                </div>
+
+                <div class="col-sm-4" id="Uitvlakken">
+                    <div class="thumbnail">
+                        <img src="assets/img/img15.jpg" alt="Paris" width="400" height="300">
+                        <p><strong>Uitvlakken</strong></p>
+                        <p>Yes, we built Paris</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="thumbnail">
+                        <img src="assets/img/img15.jpg" alt="New York" width="400" height="300">
+                        <p><strong>New York</strong></p>
+                        <p>We built New York</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="thumbnail">
+                        <img src="assets/img/img15.jpg" alt="San Francisco" width="400" height="300">
+                        <p><strong>San Francisco</strong></p>
+                        <p>Yes, San Fran is ours</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-sm-2">
-        </div>
-    </div>
-</div>
 
 <?php include("inc/parts/footer.php"); ?>
--- overons.php
+++ overons.php
@@ -21,59 +21,9 @@ include("inc/parts/header.php");
                 </div>
             </div>
         </div>
-<p><div class="container" style="border-bottom-style: solid;"></div> </p>
-        <div id="diensten" class="container text-center">
-            <h2>DIENSTEN</h2><br>
-            <h4>De volgende diensten bieden wij aan</h4>
-            <div class="row text-center slideanim">
-                <div class="col-sm-4" id="Grondwerk">
-                    <div class="thumbnail">
-                        <img src="assets/img/img3.jpg" alt="Paris" width="400" height="300">
-                        <p><strong>Grondwerk</strong></p>
-                        <p>Met grondwerk word</p>
-
-                    </div>
-                </div>
-                <div class="col-sm-4" id="Riolering">
-                    <div class="thumbnail">
-                        <img src="assets/img/img8.jpg" alt="New York" width="400" height="300">
-                        <p><strong>Riolering</strong></p>
-                        <p>We built New York</p>
-                    </div>
-                </div>
-                <div class="col-sm-4" id="Machinalebestrating">
-                    <div class="thumbnail">
-                        <img src="assets/img/img13.jpg" alt="San Francisco" width="400" height="300">
-                        <p><strong>Machinale bestrating</strong></p>
-                        <p>Yes, San Fran is ours</p>
-                    </div>
-                </div>
-
-                <div class="col-sm-4" id="Uitvlakken">
-                    <div class="thumbnail">
-                        <img src="assets/img/img15.jpg" alt="Paris" width="400" height="300">
-                        <p><strong>Uitvlakken</strong></p>
-                        <p>Yes, we built Paris</p>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="thumbnail">
-                        <img src="assets/img/img15.jpg" alt="New York" width="400" height="300">
-                        <p><strong>New York</strong></p>
-                        <p>We built New York</p>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="thumbnail">
-                        <img src="assets/img/img15.jpg" alt="San Francisco" width="400" height="300">
-                        <p><strong>San Francisco</strong></p>
-                        <p>Yes, San Fran is ours</p>
-                    </div>
-                </div>
-            </div><br>
+<br>
         </div>
 
-        <?php include ("inc/parts/contact.php") ?>
-		<?php include("inc/parts/footer.php"); ?>
+        <?php include("inc/parts/footer.php"); ?>
     </body>
 </html>
