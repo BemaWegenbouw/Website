6f29b5c13d3e055272384848b006f8efdd52b260
Josha van der Heide <hevadejo@outlook.com>
2016-11-21 13:29:33 +0100

    Grondwerk include fix

--- grondwerk.php
+++ grondwerk.php
@@ -9,22 +9,9 @@ include("inc/parts/header.php");
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Bema Wegenbouw BV</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../assets/css/bootstrap.css">
-        <link rel="stylesheet" href="../assets/css/custom.css">
-
-    </head>
-    <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
-        <!-- Container voor diensten (een portolio class)-->
-        <br>
         <div class="container"id="Grondwerk">
             <h2 class="text-center">Grondwerk</h2><br>
-            <div class="row slideanim">
+            <div class="row">
                 <div class="col-sm-4 col-md-4" style="background-color:yellow;">
                     <img src="../assets/img/img3.jpg" alt="grondwerk" width="100%" >
                 </div>
@@ -35,9 +22,5 @@ include("inc/parts/header.php");
             </div>
         </div>   
 		<br>
-	<!-- Container (Contact Section) -->
-		<?php include ("contact.php") ?>
-	<!-- Header -->
-		<?php include("footer.php"); ?>
-</body>
-</html>
+
+<?php include("inc/parts/footer.php"); ?>
