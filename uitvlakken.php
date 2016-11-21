f285e34b1beb1a17d77c8b12ad6a81ffb5e80e56
Josha van der Heide <hevadejo@outlook.com>
2016-11-21 13:30:39 +0100

    Uitvlakken include fix

--- uitvlakken.php
+++ uitvlakken.php
@@ -1,23 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Bema Wegenbouw BV</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../assets/css/bootstrap.css">
-        <link rel="stylesheet" href="../assets/css/custom.css">
+<?php
 
-    </head>
-    <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+//Bema Wegenbouw BV Website
+//Copyright 2016
 
-        <!-- Navbar -->
-        <?php include("navbar.php"); ?>
-		<!-- Header -->
-		<?php include("header.php"); ?>
-        <!-- Container voor diensten (een portolio class)-->
-        <!-- Container voor diensten (een portolio class)-->
-        <br>
+require_once("inc/engine.php");
+$page = "uitvlakken";
+include("inc/parts/header.php");
 
+?>
         <div class="container"id="Uitvlakken">
             <h2 class="text-center">Uitvlakken</h2><br>
             <div class="row slideanim">
@@ -33,9 +23,5 @@
     </div>
 </div>
 <br>
-	<!-- Container (Contact Section) -->
-		<?php include ("contact.php") ?>
-	<!-- Header -->
-		<?php include("footer.php"); ?>
-</body>
-</html>
+
+<?php include("footer.php"); ?>
