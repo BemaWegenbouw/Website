5879c552632730ef50825f054ed008381c5a37cc
albert810 <ahwildeboer@gmail.com>
2016-11-23 10:27:51 +0100

    inlog functie wachtwoord in verhouding met username
    inlogfunctie waar true or false uit komt  (zie uitleg functie in
    bestand) databasekoppeling van de username en password data wat hij gaat
    vergelijken met de data in de database

--- inc/controller/user.controller.php
+++ inc/controller/user.controller.php
@@ -1,13 +1,46 @@
 <?php
 
-class user {
 
-	public function checkPassword($user, $pass) {
+
+class user {
+    
+    
+    //je gebruikt de functie:   $trueorfalse    =  checkpassword($user,$pass)   dan komt er true of false uit
+                            //       de functie controlleerd of de combinatie met pass en username true is
+                        //                       
+    
+    public function checkPassword($user, $pass) {
+        global $pdo;
+global $config;
+global $_POST;
+include("inc/controller/db.controller.php");
+include("inc/config.php");
+$stmt=$pdo->prepare ("select * from staff where username = '$user'  AND password = '$pass'");
+        $stmt->execute();
+        
+
+
+
+        while ($row=$stmt->fetch())
+        { $usernam=$row["username"];
+            $passw=$row["password"];
+                $rank=$row["rank_id"];
+       
+       
+
+         if ($passw==$pass && $usernam==$user){return true;}else {return false;}
+
+         
+         
+
+} 
         
         
         
-    }
+    }}
+
 	
-}
+
+
 
 ?>
