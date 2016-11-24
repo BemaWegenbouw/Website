250ce2cdd6c527c23043028f183f62a4fd5a6551
Josha van der Heide <hevadejo@outlook.com>
2016-11-24 16:52:25 +0100

    Footer fix

--- staff/addstaff.php
+++ staff/addstaff.php
@@ -14,21 +14,7 @@ if (isset($_POST) && !empty($_POST)) { //Check of er een post is
 }
 
 ?>
-        <!-- Page Content -->
-        <div id="page-wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">Blank</h1>
-                    </div>
-                    <!-- /.col-lg-12 -->
-                </div>
-                <!-- /.row -->
-            </div>
-            <!-- /.container-fluid -->
-</div>
-        <!-- /#page-wrapper -->
-    </div>
-    <!-- /#wrapper -->
+
+
 
 <?php include("../inc/parts/staff-footer.php"); ?>
