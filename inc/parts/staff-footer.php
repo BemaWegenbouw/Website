d28e13cebd257572fd47c81b30d99169ab4045fe
joeyb276 <j.benning@hotmail.nl>
2016-11-24 16:40:57 +0100

    iewujlidwjliwijlbewlijewijnewjine

---
+++ inc/parts/staff-footer.php
@@ -0,0 +1,28 @@
+
+
+    
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<script src="../assets/js/ie10-viewport-bug-workaround.js"></script>
+	<script src="../assets/js/jquery.min.js"></script>
+	<script src="../assets/js/bootstrap.js"></script>
+	<script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="vendor/raphael/raphael.min.js"></script>
+    <script src="vendor/morrisjs/morris.min.js"></script>
+    <script src="data/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../assets/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
