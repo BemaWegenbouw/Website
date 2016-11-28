329546abb9a9fdd4fc070c5a3ff2df502cd55e36
Pietervurk <p.h.vanurk@hotmail.nl>
2016-11-28 13:55:03 +0100

    form toegevoegd
    form gemaakt in declaration.php

--- staff/declaration.php
+++ staff/declaration.php
@@ -1,33 +1,40 @@
 <?php
-
 //Bema Wegenbouw BV Website
 //Copyright 2016
 
 $page = "staff-declaration";
 require_once("../inc/engine.php");
 include("../inc/parts/staff-header.php");
-
 ?>
-        <!-- Page Content -->
-        <div id="page-wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">Urendeclaratie</h1>
-                    </div>
-                    <!-- /.col-lg-12 -->
-                </div>
-                <!-- /.row -->
-            </div>
-            <!-- /.container-fluid -->
-        </div>
-        <!-- /#page-wrapper -->
+<!-- Page Content -->
+<div id="page-wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Urendeclaratie</h1>
+                <div>
+                    <form method="Post" action="declaration.php">
+                        datum: <input type="text" name="date" placeholder="dd-mm-jjjj" required><br>
+                        begintijd: <input type="text" name="start_date" placeholder="00:00" required>
+                        eindtijd: <input type="text" name="end_date" placeholder="00:00" required><br>
+                        <input type="checkbox" name="break" value="false"> ik heb geen pauze gehad.<br>
+                        pauze: <input type="text" name ="break_time" placeholder="in minuten"><br>
+                        <button class="btn btn-default pull-left" type="submit" name="submit" value="Submit">verzenden</button>
 
+                    </form>
+                </div>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
     </div>
-    <!-- /#wrapper -->
+    <!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
 
 <?php
-
 include("../inc/parts/staff-footer.php");
-
 ?>
