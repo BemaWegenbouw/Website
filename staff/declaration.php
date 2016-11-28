15ae143200b5dad6c3f8eca828c55681ccfcf968
Pietervurk <p.h.vanurk@hotmail.nl>
2016-11-28 14:28:41 +0100

    opmaak form declaration.php
    opmaak form declaration.php

--- staff/declaration.php
+++ staff/declaration.php
@@ -12,14 +12,30 @@ include("../inc/parts/staff-header.php");
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">Urendeclaratie</h1>
-                <div>
+                <div class="col-sm-4">
                     <form method="Post" action="declaration.php">
-                        datum: <input type="text" name="date" placeholder="dd-mm-jjjj" required><br>
-                        begintijd: <input type="text" name="start_date" placeholder="00:00" required>
-                        eindtijd: <input type="text" name="end_date" placeholder="00:00" required><br>
-                        <input type="checkbox" name="break" value="false"> ik heb geen pauze gehad.<br>
-                        pauze: <input type="text" name ="break_time" placeholder="in minuten"><br>
-                        <button class="btn btn-default pull-left" type="submit" name="submit" value="Submit">verzenden</button>
+<!--                        datum: <input type="text" name="date" placeholder="dd-mm-jjjj" required><br>-->
+                        <div class="col-sm-12 form-group">
+                            datum:<input class="form-control" id="date" name="date" placeholder="dd-mm-jjjj" type="text" required>
+                        </div>
+                        <div class="col-sm-6 form-group">
+                            begintijd:<input class="form-control" id="start_date" name="start_date" placeholder="(00:00)" type="text" required>
+                        </div>
+                        <div class="col-sm-6 form-group">
+                            eindtijd:<input class="form-control" id="end_date" name="end_date" placeholder="(00:00)" type="text" required>
+                        </div>
+                        <div class="col-sm-12 form-group">
+                            pauze:<input class="form-control" id="break" name="break" placeholder="(in minuten)" type="text" required>
+                        </div>
+                        <div class="col-sm-12 form-group">
+                            <button class="btn btn-default pull-left" type="submit" name="submit" value="Submit">verzenden</button>
+                        </div>
+                        <!--
+                                                begintijd: <input type="text" name="start_date" placeholder="00:00" required>
+                                                eindtijd: <input type="text" name="end_date" placeholder="00:00" required><br>
+                                                <input type="checkbox" name="break" value="false"> ik heb geen pauze gehad.<br>
+                                                pauze: <input type="text" name ="break_time" placeholder="in minuten"><br>
+                                                <button class="btn btn-default pull-left" type="submit" name="submit" value="Submit">verzenden</button>-->
 
                     </form>
                 </div>
