fa71cf51006726245413b12dd15fac63ab73fa4c
albert810 <ahwildeboer@gmail.com>
2016-12-04 17:02:26 +0100

    availability
    availability

--- staff/timetable.php
+++ staff/timetable.php
@@ -5,6 +5,15 @@
 $page = "staff-calendar";
 require_once("../inc/engine.php");
 include("../inc/parts/staff-header.php");
+
+
+
+$uid = $_SESSION["uid"];
+//$calendar->SetStartTime($uid, '2016-11-24', '10:00');
+
+
+
+
 ?>
 
 
@@ -14,11 +23,23 @@ include("../inc/parts/staff-header.php");
 <div id="page-wrapper" class="col-sm-12">
     <iframe src="calendar/timetableengine.php" frameborder="0" marginheight="0" marginwidth="0" scrolling="no"></iframe> 
 
-
+<?php $calendar->GetAvailability($uid);  ?>
     <!-- /.col-lg-12 -->
+    <div> <form method="post" action="calendar-process.php">
+        
+          
+      Begintijd <input type="text"     name="start_time"        value="08:00">
+      Eindtijd <input type="text"      name="end_time"  value="09:00">
+        date <input type="date" name="workingdate"  >
+        <input type="submit"             name="knopcalendar"        value="verstuur">  </form> </div> 
+    
+    
+    <?php   
+  
+     
+//       if (isset($knopcalendar)){
 
-
-
-    <?php
+    
+    
     include("../inc/parts/staff-footer.php");
     
