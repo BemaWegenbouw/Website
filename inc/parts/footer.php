497058e4c1c31623e51a75f2be450313b1b6c785
joeyb276 <j.benning@hotmail.nl>
2016-12-09 15:32:06 +0100

    contact plus dashboard

--- inc/parts/footer.php
+++ inc/parts/footer.php
@@ -1,8 +1,9 @@
-<p />
 
-<footer class="text-center" style="margin-bottom: -50px; background-color: lightgrey; border-radius: 10px; text-align: center; align: center; margin-left: 30px; margin-right: 30px;">
+<footer class="footers">
     &copy; <?php echo date('Y'); ?> <a href="http://www.bemawegenbouw.nl/">Bema Wegenbouw BV</a>
-    <br />KvK: 66369711 | BTW-nummer: NL856518232B01 | Gevestigd te Enschede
+	Aamsveenweg 294 | 7536 PB Enschede Nederland | Tel:0031(0)53 4779223 |  Tel mobiel: 0031(0)6 3471605<br>
+    Email: info@bemawegenbouw.nl | Website: www.bemawegenbouw.nl |K.v.k Nr: 66369711 | BTW Nr: 8564.18.232.B01<br>
+	Bankverbinding: SNS Bank IBAN-Nr. NL 78 SNSB 08712633 27 BIC-Nr. SNSBNL2A | KNAB Bank: IBAN-Nr 12 KNAB 0255 2760 44 BIC-Nr. KNABNL2H
 </footer>
 
 <?php //Javascript import ?>
@@ -10,43 +11,44 @@
 <script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/bootstrap.js"></script>
 
-<?php //Google Maps script ?>
+	<?php if($page == "contact") {?>
+		<script>
+		//Google Maps script  
+		  $(document).ready(function () {
+		// Add smooth scrolling to all links in navbar + footer link
+				$(".navbar a, footer a[href='#myPage']").on('click', function (event) {
+					// Make sure this.hash has a value before overriding default behavior
+					if (this.hash !== "") {
+						// Prevent default anchor click behavior
+						event.preventDefault();
 
-<script>
-    $(document).ready(function () {
-// Add smooth scrolling to all links in navbar + footer link
-        $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
-            // Make sure this.hash has a value before overriding default behavior
-            if (this.hash !== "") {
-                // Prevent default anchor click behavior
-                event.preventDefault();
+						// Store hash
+						var hash = this.hash;
 
-                // Store hash
-                var hash = this.hash;
+						// Using jQuery's animate() method to add smooth page scroll
+						// The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+						$('html, body').animate({
+							scrollTop: $(hash).offset().top
+						}, 900, function () {
 
-                // Using jQuery's animate() method to add smooth page scroll
-                // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top
-                }, 900, function () {
+							// Add hash (#) to URL when done scrolling (default click behavior)
+							window.location.hash = hash;
+						});
+					} // End if
+				});
 
-                    // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = hash;
-                });
-            } // End if
-        });
+				$(window).scroll(function () {
+					$(".slideanim").each(function () {
+						var pos = $(this).offset().top;
 
-        $(window).scroll(function () {
-            $(".slideanim").each(function () {
-                var pos = $(this).offset().top;
-
-                var winTop = $(window).scrollTop();
-                if (pos < winTop + 600) {
-                    $(this).addClass("slide");
-                }
-            });
-        });
-    })
-</script>
+						var winTop = $(window).scrollTop();
+						if (pos < winTop + 600) {
+							$(this).addClass("slide");
+						}
+					});
+				});
+			})
+		</script>
+		<?php } ?>
 </body>
 </html>
