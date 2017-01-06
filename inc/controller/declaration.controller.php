7539348867fa9d89983aa4f2f772f48f7d23dace
albert810 <ahwildeboer@gmail.com>
2017-01-06 22:13:27 +0100

    commetaar bij code en paar dingen of vage dingen aangepast
    Commetaar bij code en paar dingen of paar vage dingen aangepast

--- inc/controller/declaration.controller.php
+++ inc/controller/declaration.controller.php
@@ -3,19 +3,20 @@
 
 class declaration {
 
+//------------------------toevoegen van een declaratie : medewerker----------------------------------    
     public function insert($userid, $date, $start_time, $end_time, $break) {
         global $pdo;
-        $sth = $pdo->prepare("SELECT date, uid
-                                            FROM declaration
-                                             WHERE uid=:uid and date=:date");
-        $sth->bindParam(':uid', $userid, PDO::PARAM_STR);
-        $sth->bindParam(':date', $date, PDO::PARAM_STR);
+        //--laat zien of de ingevoerde datum in combinatie met UID bestaat, anders komt er een primairy key clash , hier selecteer je alles --
+        $sth = $pdo->prepare("SELECT date, uid FROM declaration WHERE uid=:uid and date=:date");//maak de query klaar
+        $sth->bindParam(':uid', $userid, PDO::PARAM_STR);//Vervang :uid naar $userid variabele  
+        $sth->bindParam(':date', $date, PDO::PARAM_STR);//Vervang :date naar $date variabele  
         $sth->execute(); //Voer de query uit
         
+        
 		$row = $sth->fetchall(PDO::FETCH_ASSOC);         
-		$aantal = count($row); 
+		$aantal = count($row); //kijk hoe veel records er uit komen
 		 
-        if ($aantal == 0) {
+        if ($aantal == 0) {//als er 0 records uit komen (dat betekent dat hij niet bestaat) dan voert hij de insert declaratie in.
             //Zoek naar $pdo buiten deze functie
             $sth = $pdo->prepare("INSERT INTO declaration (uid, date, start_time, end_time, break) values (:uid, :date, :start_time, :end_time, :break)"); //Maak de query klaar
             $sth->bindParam(':uid', $userid, PDO::PARAM_STR);
@@ -25,35 +26,20 @@ class declaration {
             $sth->bindParam(':break', $break, PDO::PARAM_STR);
             $sth->execute(); //Voer de query uit
             return(true);
-        } else {
-            print("<script type='text/javascript'>noty({text: 'deze dag is al gedeclareerd', type: 'error', layout: 'top', theme: 'relax', timeout: 10000});</script>");
+        } else {// als hij niet bestaat komt er een foutmelding
+            print("<script type='text/javascript'>noty({text: 'deze dag is al gedeclareerd', type: 'error', layout: 'top', theme: 'relax', timeout: 10000});</script>");//foutmelding
         }
-    }
-
-//    public function declist() {
-//
-//        global $pdo; //Zoek naar $pdo buiten deze functie
-//        $sth = $pdo->prepare("SELECT * FROM declaration"); //Maak de query klaar
-//        $sth->execute(); //Voer de query uit
-//
-//        $row = $sth->fetch(PDO::FETCH_ASSOC);
-//        print_r($row); //Einde van de tabel
-//    }
-
+    }//einde functie
+    
+    
+    //-------------------------------functie maakt de tr's en de TD's voor de admin tabel aan , voor de openstaande declaraties------------------------------------
     public function declist() {
 
-
-
         global $pdo; //Zoek naar $pdo buiten deze functie
-        $sth = $pdo->prepare("SELECT first_name, last_name, date, start_time, end_time, break, d.id
-                                            FROM declaration d join staff s on d.uid = s.uid
-                                             WHERE verify IS NULL"); //query
+        $sth = $pdo->prepare("SELECT first_name, last_name, date, start_time, end_time, break, d.id  FROM declaration d join staff s on d.uid = s.uid WHERE verify IS NULL"); //query
         $sth->execute(); //Voer de query uit
 
-
-
-
-        while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {   //Creates a loop to loop through results
+        while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {   //Creates a loop to loop through results          maak de tabel aan
             echo "<tr class='gradeA'>
 			  <td>" . $row['first_name'] . "</td>
 			  <td>" . $row['last_name'] . "</td>
@@ -74,22 +60,17 @@ class declaration {
 
 			  </tr>";
         }
-    }
+    }//einde functie
 
+    //maakt tr's en td's aan voor de tabel van de admin voor goedgekeurde declaraties.
     public function declistgoedgekeurd() {
-
-
 
         global $pdo; //Zoek naar $pdo buiten deze functie
         $sth = $pdo->prepare('SELECT first_name, last_name, date, start_time, end_time, break,verify
                                             FROM declaration d join staff s on d.uid = s.uid
                                             WHERE verify IS NOT null or verify IS NOT NULL '); //query
         $sth->execute(); //Voer de query uit
-
-
-
-
-        while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {   //Creates a loop to loop through results
+ while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {   //Creates a loop to loop through results  && maak de rijen voor de tabel aan
             echo "<tr class = 'gradeA'>
 <td>" . $row['first_name'] . "</td>
 <td>" . $row['last_name'] . "</td>
@@ -100,47 +81,45 @@ class declaration {
 <td class = " . $row["verify"] . ">" . $row['verify'] . "</td>
 </tr>";
         }
-    }
-
+    }//einde functie
+    
+    //---------------------haalt de gegevens op van de medewerkers------------------------------
     public function get($id, $value) {
         global $pdo; //Zoek naar $pdo buiten deze functie
         $sth = $pdo->prepare("SELECT * FROM declaration WHERE id = :id"); //Maak de query klaar
-        $sth->bindParam(':id', $id, PDO::PARAM_STR); //Vervang :username naar $user variabele
+        $sth->bindParam(':id', $id, PDO::PARAM_STR); //Vervang :id naar $id variabele
         $sth->execute(); //Voer de query uit
         $result = $sth->fetch(PDO::FETCH_ASSOC); //Sla het resultaat op in een variabele
         return $result[$value]; //Geef resultaat terug
-    }
+    }//einde functie
 
+    //functie die bepaalde records GOED KEURT
     public function approveFree($key) {
 
         global $pdo; //Zoek naar $pdo buiten deze functie
         $sth = $pdo->prepare("UPDATE declaration SET verify = 'goedgekeurd' WHERE id = :id"); //query
-        $sth->bindparam(':id', $key, PDO::PARAM_STR);
+        $sth->bindparam(':id', $key, PDO::PARAM_STR);//vervang variable :id naar $key
         $sth->execute(); //Voer de query uit
-    }
+    }//einde functie
 
+    //Functie die bepaalde records  FOUT KEURT
     public function denyFree($key) {
 
         global $pdo; //Zoek naar $pdo buiten deze functie
         $sth = $pdo->prepare("UPDATE declaration SET verify = 'afgekeurd' WHERE id = :id"); //query
-        $sth->bindparam(':id', $key, PDO::PARAM_STR);
+        $sth->bindparam(':id', $key, PDO::PARAM_STR);//vervang variable :id naar $key
         $sth->execute(); //Voer de query uit
-    }
-
-//Einde van de Staff Lijst functie.
+    }//einde functie
+ 
+    
+    //------functie die td's en tr's maakt voor de OPENSTAANDE declaraties---------
     public function decListCompleetUnapproved($userid) {
 
         global $pdo; //Zoek naar $pdo buiten deze functie
-        $sth = $pdo->prepare("SELECT first_name, last_name, date, start_time, end_time, break
-FROM declaration d join staff s on d.uid = s.uid
-WHERE d.uid = :uid and verify is null"); //query
-        $sth->bindParam(':uid', $userid, PDO::PARAM_STR);
-        $sth->execute(); //Voer de query uit
-
-
-
-
-        while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {   //Creates a loop to loop through results
+        $sth = $pdo->prepare("SELECT first_name, last_name, date, start_time, end_time, break FROM declaration d join staff s on d.uid = s.uid WHERE d.uid = :uid and verify is null"); //query
+        $sth->bindParam(':uid', $userid, PDO::PARAM_STR);//vervang variable :uid naar $userid
+        $sth->execute(); //Voer de query uit     
+        while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {   //Creates a loop to loop through results  
             echo "<tr class = 'gradeA'>
             <td>" . $row['first_name'] . "</td>
             <td>" . $row['last_name'] . "</td>
@@ -149,22 +128,20 @@ WHERE d.uid = :uid and verify is null"); //query
             <td>" . $row['end_time'] . "</td>
             <td>" . $row['break'] . "</td>
             </tr>";
-        }
-    }
+        }//einde while
+    }//einde functie
 	
+    
+         //-----------functie die td's en tr's maakt voor de GESLOTEN declaraties----------------
 	  public function decListCompleetApproved($userid) {
 
         global $pdo; //Zoek naar $pdo buiten deze functie
-        $sth = $pdo->prepare("SELECT first_name, last_name, date, start_time, end_time, break
-FROM declaration d join staff s on d.uid = s.uid
-WHERE d.uid = :uid and verify is not null "); //query
-        $sth->bindParam(':uid', $userid, PDO::PARAM_STR);
+        $sth = $pdo->prepare("SELECT first_name, last_name, date, start_time, end_time, break FROM declaration d join staff s on d.uid = s.uid WHERE d.uid = :uid and verify is not null "); //query
+        $sth->bindParam(':uid', $userid, PDO::PARAM_STR);    //vervang variabele :uid met $userid
+
         $sth->execute(); //Voer de query uit
 
-
-
-
-        while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {   //Creates a loop to loop through results
+        while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {   //Creates a loop to loop through results print de tr en td"s van de tabel
             echo "<tr class = 'gradeA'>
             <td>" . $row['first_name'] . "</td>
             <td>" . $row['last_name'] . "</td>
@@ -174,9 +151,9 @@ WHERE d.uid = :uid and verify is not null "); //query
             <td>" . $row['break'] . "</td>
             </tr>";
         }
-    }
+    }//einde functie
 
 }
 
-$declaration = new declaration;
+$declaration = new declaration;//maakt de class aan
 ?>
